4c02fbb7ee0862c19a0ab67916313778d1acacad
Òscar Pelegrina <oscarpelegrina99@gmail.com>
2025-11-02 18:30:50 +0100

    init: React + TS client base

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div>
+      <h1>Bienvenido a MarketAnalyzerClient</h1>
+      <p>Tu aplicación está lista para comenzar.</p>
+    </div>
+  )
+}
+
+export default App
